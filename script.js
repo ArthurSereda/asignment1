9164834b0ce2ccdc1065353424867b5bb070b912
ArthurSereda <neonbetcy@gmail.com>
2019-12-04 17:08:24 +0200

    Добавив цикл для введення витрат

--- script.js
+++ script.js
@@ -2,21 +2,40 @@ let money = prompt("Ваш бюджет на місяць?");
 let time = prompt("Введіть дату в форматі YYYY-MM-DD");
 
 let appData = {
-    budget: money,
-    expences: {},
-    optionalExpences: {},
-    income: [],
-    timeData: time,
-    saving: false
+  budget: money,
+  expenses: {},
+  optionalExpenses: {},
+  income: [],
+  timeData: time,
+  saving: false
 };
 
-let a1 = prompt("Введіть обов'язкову статтю розходів в цьому місяці"),
-    a2 = prompt("В скільки обійдуться розходи?"),
-    a3 = prompt("Введіть обов'язкову статтю розходів в цьому місяці"),
-    a4 = prompt("В скільки обійдуться розходи?");
+for (let i = 0; i < 2; i++) {
+  let a = prompt("Введіть обов'язковий список витрат цього місяця"),
+    b = prompt("В скільки $$ обійдуться розходи?");
+      if(typeof(a) === "string" &&
+        typeof(a) != null &&
+        a != "" &&
+        a.length < 50 &&
+        typeof(b) != null &&
+        typeof(b) === "string" &&
+        b != "") {
+          appData.expenses[a] = b;
+        } else {
+          i -= 1;
+        }
+}
 
-appData.expences.a1 = a2;
-appData.expences.a3 = a4;
+appData.moneyPerDay = Math.round(appData.budget / 30);
 
-alert(appData.budget / 30);
+alert(`Щоденні витрати: ${appData.moneyPerDay}`);
 
+if (appData.moneyPerDay < 100) {
+  console.log("Мінімальний рівень достатку");
+} else if (appData.moneyPerDay > 100 && appData.moneyPerDay < 2000) {
+  console.log("Середній рівень достатку");
+} else if (appData.moneyPerDay > 2000) {
+  console.log("Високий рівень достатку");
+} else {
+  console.log("Відбулася якась помилка");
+}
